56ede04116418e05547028a18ff2845e83cc2ddb
Vitaly Liber <liber@sibext.com>
2016-08-26 19:20:58 +0700

    get json from input

--- app/assets/javascripts/rails_admin_advanced_json_editor/rails_admin_advanced_json_editor.js
+++ app/assets/javascripts/rails_admin_advanced_json_editor/rails_admin_advanced_json_editor.js
@@ -7,36 +7,8 @@ $(function() {
   // editor = new JSONEditor( $('#advanced-json-editor') );
 
   var editor = new JSONEditor(document.getElementById('advanced-json-editor'),{
-    schema: {
-      type: "object",
-      title: "Car",
-      properties: {
-        make: {
-          type: "string",
-          enum: [
-            "Toyota",
-            "BMW",
-            "Honda",
-            "Ford",
-            "Chevy",
-            "VW"
-          ]
-        },
-        model: {
-          type: "string"
-        },
-        year: {
-          type: "integer",
-          enum: [
-            1995,1996,1997,1998,1999,
-            2000,2001,2002,2003,2004,
-            2005,2006,2007,2008,2009,
-            2010,2011,2012,2013,2014
-          ],
-          default: 2008
-        }
-      }
-    }
+    theme: 'bootstrap3',
+    schema: JSON.parse( $('#advanced-json-editor input').val() )
   });
 
   // Hook up the submit button to log to the console
